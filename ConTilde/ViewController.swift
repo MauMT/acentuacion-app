18176d2b1968ec8039bad221c05d3b67bb6676f5
Mauricio Martinez Toledo <mauriciomtzt@hotmail.com>
2021-04-16 15:39:14 -0700

    splash screen

--- ConTilde/ViewController.swift
+++ ConTilde/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   //let timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(cambiarLetra), userInfo: nil, repeats: true)
+    
+    @IBOutlet weak var letraAnimada: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    var contador = 0
+    /*@IBAction func cambiarLetra(){
+        //áéíóú
+        switch contador{
+        case 0:
+            letraAnimada.text! = "á"
+            contador+=1
+        case 1:
+            letraAnimada.text! = "é"
+            contador+=1
+        case 2:
+            letraAnimada.text! = "í"
+            contador+=1
+        case 3:
+            letraAnimada.text! = "ó"
+            contador+=1
+        case 4:
+            letraAnimada.text! = "ú"
+            contador=0
+        default:
+            letraAnimada.text! = "á"
+        }
+        
+    }*/
 }
 
