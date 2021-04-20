19599f58c9accf7dc9f150ce5a1cc26748b9efae
Mauricio Martinez Toledo <mauriciomtzt@hotmail.com>
2021-04-20 10:44:59 -0700

    swipe modo de juego

---
+++ ViewControllerClassic.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerClassic.swift
+//  ConTilde
+//
+//  Created by user189095 on 4/20/21.
+//
+
+import UIKit
+
+class ViewControllerClassic: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
