7bbcd1a5458c0fedadbcc70ab72a4a1e66e4af07
Mauricio Martinez Toledo <mauriciomtzt@hotmail.com>
2021-04-20 10:59:12 -0700

    modo de juego

---
+++ ConTilde/README.md
@@ -0,0 +1,3 @@
+# Con Tilde
+### Repositorio para la aplicación "Con Tilde" para la materia de Proyectos de desarrollo para dispositivos móviles
+Aplicación para iOS usando iOS Storyboards
---
+++ ConTilde/ViewControllerModoDeJuego.swift
@@ -0,0 +1,92 @@
+//
+//  ViewControllerModoDeJuego.swift
+//  ConTilde
+//
+//  Created by user189095 on 4/20/21.
+//
+
+import UIKit
+
+class ViewControllerModoDeJuego: UIViewController {
+
+    @IBOutlet weak var lbModoDeJuego: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        
+            downSwipe.direction = .down
+            upSwipe.direction = .up
+            leftSwipe.direction = .left
+        rightSwipe.direction = .right
+        
+        view.addGestureRecognizer(downSwipe)
+        view.addGestureRecognizer(upSwipe)
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
+        
+    }
+    
+    @objc func handleSwipes(_ sender: UISwipeGestureRecognizer)
+    {
+        if sender.direction == .down
+        {
+           print("Swipe down")
+            if let nav = self.navigationController {
+                nav.popViewController(animated: true)
+            } else {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+
+        if sender.direction == .up
+        {
+           print("Swipe up")
+            
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            if lbModoDeJuego.text! == "Classic" {
+                let resultViewController = storyBoard.instantiateViewController(withIdentifier: "Classic") as! ViewControllerClassic
+
+                self.present(resultViewController, animated:true, completion:nil)
+            }
+            else if lbModoDeJuego.text! == "Time Attack" {
+                let resultViewController = storyBoard.instantiateViewController(withIdentifier: "Time Attack") as! ViewControllerTimeAttack
+
+                self.present(resultViewController, animated:true, completion:nil)
+            }else{
+                let resultViewController = storyBoard.instantiateViewController(withIdentifier: "Zen") as! ViewControllerTimeAttack
+
+                self.present(resultViewController, animated:true, completion:nil)
+            }
+
+        }
+        
+        if sender.direction == .left {
+            
+            if lbModoDeJuego.text! == "Classic" {
+                lbModoDeJuego.text! = "Time Attack"
+            }else if lbModoDeJuego.text! == "Time Attack" {
+                lbModoDeJuego.text! = "Zen"
+            }else {
+                lbModoDeJuego.text! = "Classic"
+            }
+        }
+        
+        if sender.direction == .right {
+            
+            if lbModoDeJuego.text! == "Classic" {
+                lbModoDeJuego.text! = "Zen"
+            }else if lbModoDeJuego.text! == "Time Attack" {
+                lbModoDeJuego.text! = "Classic"
+            }else {
+                lbModoDeJuego.text! = "Time Attack"
+            }
+        }
+    }
+
+
+}
