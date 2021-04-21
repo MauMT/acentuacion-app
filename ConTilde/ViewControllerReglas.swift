f501a571f08d81b4a14ffe929628ffb1d3337b40
Mauricio Martinez Toledo <mauriciomtzt@hotmail.com>
2021-04-20 19:51:55 -0700

    swipe

---
+++ ConTilde/ViewControllerReglas.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControllerReglas.swift
+//  ConTilde
+//
+//  Created by user189095 on 4/20/21.
+//
+
+import UIKit
+
+class ViewControllerReglas: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
