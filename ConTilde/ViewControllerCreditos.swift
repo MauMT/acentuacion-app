03892483ed543d5bf3f149d9897c8777362b90f3
Marcos Quintero <a00824603@itesm.mx>
2021-05-20 16:45:41 -0600

    creditos de la app

---
+++ ConTilde/ViewControllerCreditos.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerCreditos.swift
+//  ConTilde
+//
+//  Created by Marcos Quintero on 20/05/21.
+//
+
+import UIKit
+
+class ViewControllerCreditos: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
