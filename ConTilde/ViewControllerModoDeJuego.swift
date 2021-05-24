759753061bd76229f6339f9e118513a946ef590e
Mauricio Martinez Toledo <mauriciomtzt@hotmail.com>
2021-05-23 18:19:48 -0700

    added image transition when changing gamemode

--- ConTilde/ViewControllerModoDeJuego.swift
+++ ConTilde/ViewControllerModoDeJuego.swift
@@ -121,13 +121,17 @@ class ViewControllerModoDeJuego: UIViewController {
             
             if lbModoDeJuego.text! == "Classic" {
                 lbModoDeJuego.text! = "Time Attack"
-                imgModo.image! = UIImage(named: "time-white.jpg")!
+                ///imgModo.image! = UIImage(named: "time-white.jpg")!
+                UIView.transition(with: imgModo, duration: 0.5, options: .transitionCrossDissolve, animations: {self.imgModo.image=UIImage(named: "time-white.jpg")!}, completion: nil)
             }else if lbModoDeJuego.text! == "Time Attack" {
                 lbModoDeJuego.text! = "Zen"
-                imgModo.image! = UIImage(named: "zen-blanco.jpg")!
+                ///imgModo.image! = UIImage(named: "zen-blanco.jpg")!
+                UIView.transition(with: imgModo, duration: 0.5, options: .transitionCrossDissolve, animations: {self.imgModo.image=UIImage(named: "zen-blanco.jpg")!}, completion: nil)
+                
             }else {
                 lbModoDeJuego.text! = "Classic"
-                imgModo.image! = UIImage(named: "classicTemp-white.jpg")!
+                ///imgModo.image! = UIImage(named: "classicTemp-white.jpg")!
+                UIView.transition(with: imgModo, duration: 0.5, options: .transitionCrossDissolve, animations: {self.imgModo.image=UIImage(named: "classicTemp-white.jpg")!}, completion: nil)
             }
         }
         
